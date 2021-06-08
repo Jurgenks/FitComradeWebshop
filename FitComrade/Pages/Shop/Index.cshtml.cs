fc88c1ccadf3c9a346c5f0eae4889c33b4080f28
Jurgenks <79445768+Jurgenks@users.noreply.github.com>
2021-06-08 13:50:17 +0200

    Sort function in shop

--- FitComrade/Pages/Shop/Index.cshtml.cs
+++ FitComrade/Pages/Shop/Index.cshtml.cs
@@ -15,20 +15,42 @@ namespace FitComrade.Pages.Shop
         public List<Product> Products = new List<Product>();
         private readonly Data.FitComradeContext _context;
         [BindProperty(SupportsGet = true)]
-        public string SearchString { get; set; }        
+        public string SearchString { get; set; }
         public IndexModel(Data.FitComradeContext context)
         {
             _context = context;
         }
         public void OnGet()
         {
-            Products = _context.Products.ToList();
+            Products = _context.Products.OrderByDescending(item => item.ProductQuantity).ToList();
 
             if (!string.IsNullOrEmpty(SearchString))
             {
-                Products = _context.Products.Where(item=>item.ProductName.Contains(SearchString)).ToList();
+                Products = _context.Products.Where(item => item.ProductName.Contains(SearchString)).ToList();
             }
         }
-        
+
+        public IActionResult OnGetSort(int id)
+        {
+            if (id == 1)
+            {
+                Products = _context.Products.OrderBy(item => item.ProductID).ToList();
+            }
+            if (id == 2)
+            {
+                Products = _context.Products.OrderBy(item => item.ProductName).ToList();
+            }
+            if (id == 3)
+            {
+                Products = _context.Products.OrderBy(item => item.ProductPrice).ToList();
+            }
+            if (id == 4)
+            {
+                Products = _context.Products.OrderByDescending(item => item.ProductQuantity).ToList();
+            }
+
+            return Page();
+        }
+
     }
 }
